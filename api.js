617718d38f23ff4b2c139c258a5724e9c760bff9
Rafael Costas <rafaelcostas728@gmail.com>
2024-02-10 18:49:41 +0100

    modified:   README.md 	modified:   api.js

--- api.js
+++ api.js
@@ -1,18 +1,11 @@
-/*fetch('https://mhw-db.com/weapons/1')
-    .then(response => response.json())
-    .then(weapon => {
-        // your code here
-        console.log(weapon)
-        const imageURL = weapon.assets.image;
-        return imageURL
-    });
-  */  
+
+    
     for ( let i = 0, a=1; i < 13; i+=1, a+=90) {
       const url='https://mhw-db.com/weapons/'+a
       fetch(url)
       .then(response => response.json())
       .then(response => {
-          // your code here
+        
           try{
           const imageURL = response.assets.image;
                   // Estableciendo la URL de la imagen en el atributo src de la etiqueta img
@@ -27,7 +20,7 @@
  
 async function posicion(num,coso){
   const select = document.querySelector('select');
-  // Accede a todas las opciones dentro del select
+
   const opciones = select.options[num];
   opciones.textContent=coso;
 }
